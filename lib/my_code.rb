bfa3463f3a479944f2b9862191575585b80ae24a
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-09 02:08:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -14,8 +14,8 @@ def reduce(arr,start=nil)
     output = start
     i = 0
   else 
-    output = arr[0]
-    i = 1
+  #  output = arr[0]
+    i = 0
   end
   while i < arr.length do
     output = yield(output,arr[i])
