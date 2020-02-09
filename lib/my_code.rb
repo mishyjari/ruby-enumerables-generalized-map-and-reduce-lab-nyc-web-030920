f76ba5f2cbacddbc9a044a94740ed0180695ef36
Michelle Frattaroli <mishyjari@outlook.com>
2020-02-09 01:42:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def reduce(arr,start=0)
   i = 0
   output = start
   while i < arr.length do
-    output += arr[i] if arr[i].is_i
+    output += arr[i] if arr[i].is_a?(Integer)
     i += 1
   end
   output
